6074618272b5d8a6b507feea7d910d488612398c
박하 <97325724+buwunny@users.noreply.github.com>
2023-05-22 14:45:48 -0400

    beginnings

---
+++ main.py
@@ -0,0 +1,32 @@
+class Snuggle:
+    def __init__(self, startSize, startHunger, startThirst):
+        self.size = startSize
+        self.hunger = startHunger
+        self.thirst = startThirst
+        self.day = 0
+
+        life(day)
+
+        
+    def life(self, day, starving_days, thirsting_days):
+        return
+
+    def getNextDecision(self):
+        return
+
+
+
+class Pocket:
+    def __init__(self, nutrient, humidity, light):
+        self.nutrient = nutrient
+        self.humidity = humidity
+        self.light = light
+
+    
+
+if __name__ == "main":
+    
+    agar = [[Pocket(0, 0, 0) for _ in range(10)] for _ in range(10) ]
+
+    s = Snuggle(1, 10, 10)
+
