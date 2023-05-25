e133740e1fda3f46cd0a92d062a1c750195ead89
Bunny <rortiz6868@gmail.com>
2023-05-24 23:51:44 -0400

    basic visuals for food

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import random as rand
 
 class Snuggle:
@@ -13,16 +15,18 @@ class Snuggle:
 
         life(day)
 
-    def update(self, agar)
-        self.location.append(self.makeDecision(self.hunger, self.thirst))
+    def update(self, agar):
+        self.locations.append(self.makeDecision(self.hunger, self.thirst))
         self.update_vitals()
         
-        if self.is_alive:
-            life(day += 1)
+        # if self.is_alive:
+        #     life(day += 1)
     
     def is_alive(self, starving_days, thirsting_days):
-        if starving_days >= 7 return False 
-        if thirsting_days >= 3 return False
+        if starving_days >= 7:
+            return False 
+        if thirsting_days >= 3:
+            return False
         return True
 
     def update_vitals(self, gathered_food, gathered_moisture):
@@ -32,8 +36,8 @@ class Snuggle:
         self.hunger += gathered_food
         self.food += gathered_moisture
 
-        self.starving_days += 1 if self.hunger <= 0 else self.starving_days = 0
-        self.thirsting_days += 1 if self.thirst <= 0 else self.thirsting_days = 0
+        # self.starving_days += 1 if self.hunger <= 0 else self.starving_days = 0
+        # self.thirsting_days += 1 if self.thirst <= 0 else self.thirsting_days = 0
 
     def display(self):
         return
@@ -43,45 +47,24 @@ class Snuggle:
         return
 
     def find_highest_desireability(self):
-        for l in self.locations:
+        # for l in self.locations:
              
         return
 
 
 class Section:
-    def __init__(self, nutrient, water, light):
-        self._nutrient = nutrient
-        self._water = water
-        self.light = light
-        self._source = False
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.nutrient = 0
+        self.water = 0
+        self.light = 0
+        self.source = False
         self.contains_bacteria = False
 
     def harvest(self):
         return
     
-    @property
-    def nutrient(self):
-        return self._x
-
-    @nutrient.setter
-    def nutrient(self, n):
-        self._nutrient = n
-
-    @property
-    def water(self):
-        return self._x
-
-    @water.setter
-    def water(self, w):
-        self._water = w
-
-    @property
-    def source(self):
-        return self.source
-    
-    @source.setter
-    def source(self, s):
-        self._source = s
 
 
 """
@@ -93,16 +76,39 @@ C 0.2 70%
 D 0.1 60%
 """
 class Agar:
-    def __init__(self):
-        self.agar = [[Section(0, 0, 0) for _ in range(20)] for _ in range(20)]
+    def __init__(self, size):
+        self.size = size
+        self.agar = [[Section(i, j) for i in range(self.size)] for j in range(self.size)]
 
     def add_food(self, n):
-        for i _ in range(n):
-            self.agar[rand.randint(0, len(agar))][rand.randint(0, len(agar[]))].nutrient(100)
+        l = self.size-1
+        for _ in range(0, n):
+            x = rand.randint(0, l)
+            y = rand.randint(0, l)
+            self.agar[y][x].nutrient = 100
+            for i in self.get_adjacent(x, y):
+                if i.nutrient == 0:
+                    i.nutrient += 50
+                for j in self.get_adjacent(i.x, i.y):
+                    if j.nutrient == 0:
+                        j.nutrient += 25
+        self.agar[0][0].nutrient = -100
 
-    def add_water(self, n):
-        for i _ in range(n):
-            self.agar[rand.randint(0, len(agar))][rand.randint(0, len(agar[]))].water(100)
+    def see_food(self):
+        a = []
+        for row in self.agar:
+            b = []
+            for column in row:
+                if column != -100:
+                    b.append(column.nutrient)
+                else:
+                    b.append(-100)
+            a.append(b)
+        return a
+    # def add_water(self, n):
+    #     for i _ in range(n):
+    #         self.agar[rand.randint(0, len(agar))][rand.randint(0, len(agar[]))].water(100)
+
 
     def spread_resources(self, amount):
         return
@@ -111,24 +117,38 @@ class Agar:
 
     def get_adjacent(self, x, y):
         l = []
-        l.append(self.agar[y+1][x])
-        l.append(self.agar[y-1][x])
-        l.append(self.agar[y][x+1])
-        l.append(self.agar[y][x-1])
+        if y + 1 < self.size:
+            l.append(self.agar[y+1][x])
+        if y - 1 >= 0:
+            l.append(self.agar[y-1][x])
+        if x + 1 < self.size:
+            l.append(self.agar[y][x+1])
+        if x - 1 >= 0:
+            l.append(self.agar[y][x-1])
         return l
 
 
 
     
 
-if __name__ == "main":
-    agar = Agar()
-    agar.add_food(10)
-    agarf.add_water(10)
+if __name__ == "__main__":
+    agar = Agar(50)
+    
+    agar.add_food(50)
+    A = np.array(agar.see_food())
+    # agarf.add_water(10)
 
-    b = Bacteria(1, 50, 50, 500, 500)
-    while True:
-        b.update(agar)
+    # b = Bacteria(1, 50, 50, 500, 500)
+    # while True:
+    #     b.update(agar)
+
+    # H = np.array([[rand.randint(0, 100) for _ in range(0, 1000)] for _ in range(0, 1000)])
+    
+
+    plt.imshow(A, interpolation='bilinear')
+    
+    
+    plt.show()
 
 """
 o
